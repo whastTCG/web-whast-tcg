5e8f124750f0dcb7b233b5666494842717976f74
Bryan Gutierrezs <164836869+whastTCG@users.noreply.github.com>
2024-05-11 04:45:24 -0400

    Update urlGlobales.js

--- src/helper/urlGlobales.js
+++ src/helper/urlGlobales.js
@@ -1,14 +1,14 @@
-//export const Global = {
-  //  url: "https://whast-tcg-backend.onrender.com/carta/",
-    //urlUser:"https://whast-tcg-backend.onrender.com/user/",
-   // urlGlobal:"https://whast-tcg-backend.onrender.com/",
+export const Global = {
+    url: "https://whast-tcg-backend.onrender.com/carta/",
+    urlUser:"https://whast-tcg-backend.onrender.com/user/",
+    urlGlobal:"https://whast-tcg-backend.onrender.com/",
     
-//}
+}
 
 
- export const Global = {
-     url: "http://localhost:3900/carta/",
-     urlUser:"http://localhost:3900/user/",
-     urlGlobal:"http://localhost:3900/",
+// export const Global = {
+    // url: "http://localhost:3900/carta/",
+    // urlUser:"http://localhost:3900/user/",
+    // urlGlobal:"http://localhost:3900/",
     
- }
+// }
